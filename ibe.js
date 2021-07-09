a40fe2388821b7daba3cda58d8e8370ad52b2f67
mochi_toll <cathy.earring@gmail.com>
2021-07-09 15:20:28 +0900

    few commit

--- ibe.js
+++ ibe.js
@@ -1,6 +1,6 @@
 function KeyGen(){
     const userid=document.getElementsByName('userID')[0].value
-    if(userid=""){
+    if(userid===""){
         document.getElementsByName('IDshow')[0].innerText=`キーを入力してください`
     }else {
         document.getElementsByName('IDshow')[0].innerText=`${userid}でキーを生成しました`
