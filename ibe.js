5d8a99b64e497975a84b1c3f6b86b55ead8bd5b4
mochi_toll <cathy.earring@gmail.com>
2021-07-14 17:25:06 +0900

    add some file

--- ibe.js
+++ ibe.js
@@ -1,8 +1,14 @@
+mcl.init(mcl.BLS12_381).then(()=>{
+    document.getElementsByName('status')[0].innerText=`mcl.BLS12_381で設定しました。`
+})
+
 function KeyGen(){
     const userid=document.getElementsByName('userID')[0].value
+    const msg=document.getElementsByName('msg')[0].value
     if(userid===""){
         document.getElementsByName('IDshow')[0].innerText=`キーを入力してください`
     }else {
         document.getElementsByName('IDshow')[0].innerText=`${userid}でキーを生成しました`
     }
+    document.getElementsByName('msgshow')[0].innerText=`${msg}をメッセージとしました`
 }
