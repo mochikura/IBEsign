d62964f63ac1e11b0ebf2cdcc656d87af277c0e7
mochi_toll <cathy.earring@gmail.com>
2021-07-16 16:56:24 +0900

    change for nginx

--- ibe.js
+++ ibe.js
@@ -1,14 +1,15 @@
 
-
+const mcl=require("./index.js")
 
 function KeyGen(){
     const userid=document.getElementsByName('userID')[0].value
     const msg=document.getElementsByName('msg')[0].value
     document.getElementsByName('IDshow')[0].innerText=`${userid}でキーを生成しました`
+    document.getElementsByName('msgshow')[0].innerText=`${msg}をメッセージとしました`
     mcl.init(mcl.BLS12_381).then(()=>{
         document.getElementsByName('status')[0].innerText=`mcl.BLS12_381で設定しました。`
     })
-    document.getElementsByName('msgshow')[0].innerText=`${msg}をメッセージとしました`
+    
 }
 
 function SignGen(){
