4083fe4fda3204db4622a22ede517a41b2acf549
mochi_toll <cathy.earring@gmail.com>
2021-07-16 15:46:17 +0900

    add signgen

--- ibe.js
+++ ibe.js
@@ -1,16 +1,16 @@
 
-const mcl=require('./mcl.js')
+
 
 function KeyGen(){
     const userid=document.getElementsByName('userID')[0].value
     const msg=document.getElementsByName('msg')[0].value
-    if(userid===""){
-        document.getElementsByName('IDshow')[0].innerText=`キーを入力してください`
-    }else {
-        document.getElementsByName('IDshow')[0].innerText=`${userid}でキーを生成しました`
-    }
+    document.getElementsByName('IDshow')[0].innerText=`${userid}でキーを生成しました`
     mcl.init(mcl.BLS12_381).then(()=>{
         document.getElementsByName('status')[0].innerText=`mcl.BLS12_381で設定しました。`
     })
     document.getElementsByName('msgshow')[0].innerText=`${msg}をメッセージとしました`
+}
+
+function SignGen(){
+    document.getElementsByName('signgen')[0].innerText="署名を生成しました。"
 }
